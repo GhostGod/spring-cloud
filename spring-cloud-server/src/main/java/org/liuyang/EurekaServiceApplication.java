20febeb8cd83cf03bf3dbb244be21f78f332f3f5
liuyang <y_liu@hiersun.com>
2016-12-26 17:17:23 +0800

    提交spring cloud服务端

---
+++ spring-cloud-server/src/main/java/org/liuyang/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package org.liuyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//开启eureka服务
+@EnableEurekaServer
+//springBoot注解,spring在springBoot基础之上来构建项目
+@SpringBootApplication
+public class EurekaServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
